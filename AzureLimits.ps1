274516ddda6b5af697db517e4a14975ab7d40686
Wriju Ghosh <wriju_ghosh@hotmail.com>
2018-02-14 09:10:40 +0530

    NA
    First

---
+++ AzureLimits.ps1
@@ -0,0 +1,65 @@
+﻿#Add-AzureAccount
+$azurelocation = "australiaeast"
+
+#CSV output
+$outputfile = "C:\temp\AzQuota.csv"
+
+#Make an array of things to be dumped in csv file
+$arr = @() #declare the array
+
+#To find all the limit commands
+#Get-Help Get-AzureRm*Usage
+
+#VM and it's quota
+$vmquota = Get-AzureRmVMUsage -Location $azurelocation | Select Name, CurrentValue, Limit
+
+#$networkquota = 
+$networkquota = Get-AzureRmNetworkUsage -Location $azurelocation | Select Name, CurrentValue, Limit 
+
+#Storage quota
+$storagequota = Get-AzureRmStorageUsage | Select LocalizedName, CurrentValue, Limit
+
+#Loop through network and dump to an array
+$vmquota | ForEach-Object {
+    
+    $obj = New-Object PSObject 
+
+    $obj | Add-Member NoteProperty ResourceName $_.Name.LocalizedValue
+    $obj | Add-Member NoteProperty CurrentlyUsed $_.CurrentValue
+    $obj | Add-Member NoteProperty Limit $_.Limit
+    $obj | Add-Member NoteProperty Category "VM"
+
+    $arr += $obj
+}
+
+$networkquota | ForEach-Object {
+    
+    $obj = New-Object PSObject 
+
+    $obj | Add-Member NoteProperty ResourceName $_.Name.LocalizedValue
+    $obj | Add-Member NoteProperty CurrentlyUsed $_.CurrentValue
+    $obj | Add-Member NoteProperty Limit $_.Limit
+    $obj | Add-Member NoteProperty Category "Network"
+
+    $arr += $obj
+}
+
+
+$storagequota | ForEach-Object {
+    
+    $obj = New-Object PSObject 
+
+    $obj | Add-Member NoteProperty ResourceName $_.LocalizedName
+    $obj | Add-Member NoteProperty CurrentlyUsed $_.CurrentValue
+    $obj | Add-Member NoteProperty Limit $_.Limit
+    $obj | Add-Member NoteProperty Category "Storage"
+
+    $arr += $obj
+}
+
+
+#Export to CSV
+$arr | Export-Csv -Path $outputfile -NoTypeInformation -Force
+
+Invoke-Item $outputfile
+#Get-Content $outputfile
---
+++ FileList.ps1
@@ -0,0 +1,21 @@
+﻿$arr = @()
+$rootFolder = “C:\DemoScripts\”
+$a = Get-Date
+$fileDate = ($a.Day.ToString() + $a.Month.ToString() + $a.Year.ToString() + $a.Hour.ToString() + $a.Minute.ToString() + $a.Second.ToString())
+$outputFile = “c:\temp\FileList_” + $fileDate + “.csv”
+$fileExtension = “.ps1”
+ 
+Get-ChildItem -Path $rootFolder -Filter $fileExtension -recurse | ? {$_.PSIsContainer -eq $False} | % {
+
+    $obj = New-Object PSObject
+
+    $obj | Add-Member NoteProperty Name $_.Name
+    $obj | Add-Member NoteProperty Directory $_.DirectoryName.Replace($rootFolder + “\”,“”)
+
+    $arr += $obj
+}
+
+
+$arr | Export-CSV -Path $outputFile -noTypeInformation -Force 
+
+Invoke-Item $outputFile
