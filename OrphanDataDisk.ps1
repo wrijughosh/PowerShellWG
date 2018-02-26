e39a52ef535853b0f983d4f7e7e8f30128c5136a
Wriju Ghosh <wriju_ghosh@hotmail.com>
2018-02-26 15:03:52 +0530

    Changes 26 Feb

--- OrphanDataDisk.ps1
+++ OrphanDataDisk.ps1
@@ -10,7 +10,7 @@ Disclaimer: Please test before you use in your subscription.
 #>
 
 
-#Add-AzureRmAccount
+Add-AzureRmAccount
 $file = "orphan-managed-disk.csv"
 $data = @() 
 
@@ -23,13 +23,13 @@ foreach($disk in $MVHD){
 
     $obj = New-Object PSObject
 
-    $obj | Add-Member NoteProperty Name $disk.Name
-    $obj | Add-Member NoteProperty Location $disk.Location
-    $obj | Add-Member NoteProperty DiskSize $disk.DiskSizeGB
-    $obj | Add-Member NoteProperty VM $disk.ManagedBy.Split("/")[8]
-    $obj | Add-Member NoteProperty ResourceGroup $disk.ResourceGroupName
-    $obj | Add-Member NoteProperty CreatedOn $disk.TimeCreated
-    $obj | Add-Member NoteProperty OS $disk.OsType
+    $obj | Add-Member NoteProperty Name           $disk.Name
+    $obj | Add-Member NoteProperty Location       $disk.Location
+    $obj | Add-Member NoteProperty DiskSize       $disk.DiskSizeGB
+    $obj | Add-Member NoteProperty VM             $disk.ManagedBy.Split("/")[8]
+    $obj | Add-Member NoteProperty ResourceGroup  $disk.ResourceGroupName
+    $obj | Add-Member NoteProperty CreatedOn      $disk.TimeCreated
+    $obj | Add-Member NoteProperty OS             $disk.OsType
 
     $data += $obj
 }
