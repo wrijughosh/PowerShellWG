fb8f37c146883ee2b796dcaf086ad3cfb81493b9
Wriju <wriju_ghosh@hotmail.com>
2018-05-04 20:19:14 +0530

    Meetup Updates

---
+++ meetup/CreateVM-parameter.ps1
@@ -0,0 +1,33 @@
+﻿#Create VM
+Param(    
+    [string]$VMName,
+    [string]$RGName,
+    [string]$Location 
+)
+
+#Create VM
+
+$location = "eastus" #hardcoded 
+#$vmName = "vm2"
+$vmSize = "Standard_A1" #hardoced
+
+#Check if RG Exists
+Write-Host "Checking if the RG exists.." -ForegroundColor Yellow
+Get-AzureRmResourceGroup -Name $RGName -ErrorVariable notPresent `
+    -ErrorAction SilentlyContinue
+
+if ($notPresent)
+{
+    # ResourceGroup doesn't exist 
+    # Hence create a new one
+    New-AzureRmResourceGroup -Name $RGName -Location $Location
+    Write-Host "RG Created.." -ForegroundColor Green
+}
+else
+{
+    # ResourceGroup exist - do nothing.. continue
+    Write-Host "RG already exists.." -ForegroundColor Yellow
+}
+
+#then create the VM
+New-AzureRmVM -Name $VMName -ResourceGroupName $RGName -Location $location -Size $vmSize 
---
+++ meetup/ReadFromCSVNestedPS.ps1
@@ -0,0 +1,13 @@
+﻿
+#Read from CSV
+Import-Csv D:\GithubProj\PowerShellWG\meetup\VM-Input.csv |`
+    ForEach-Object {
+        
+        
+        Write-Host $_.VMName, $_.RGName, $_.Location
+
+        #call another PowerShell
+        $ScriptToRun = $PSScriptRoot+"\CreateVM-parameter.ps1" + " -VMName " + $_.VMName + " -RGName " + $_.RGName + " -Location " +$_.Location
+        
+        Invoke-Expression $ScriptToRun
+    }
